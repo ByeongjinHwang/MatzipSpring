36d95bd35a44ead32a60f52d492f2a8e9ca6bc97
taddy <taddy0818@naver.com>
2020-09-18 12:44:52 +0900

    기본 세팅 - 기존 Matzip 자료 옮기기 작업 중

--- MatZipSpring/src/main/java/com/koreait/matzip/CommonUtils.java
+++ MatZipSpring/src/main/java/com/koreait/matzip/CommonUtils.java
@@ -1,5 +1,31 @@
 package com.koreait.matzip;
 
-public class CommonUtils {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import com.oreilly.servlet.MultipartRequest;
+
+public class CommonUtils {
+	public static int getIntParameter(MultipartRequest request, String key) {
+		return parseStrToInt(request.getParameter(key));
+	}
+	
+	public static int getIntParameter(HttpServletRequest request, String key) {
+		return parseStrToInt(request.getParameter(key));
+	}
+	
+	public static int parseStrToInt(String str) {
+		return parseStrToInt(str, 0);
+	}
+	
+	public static int parseStrToInt(String str, int defNo) {
+		try {
+			return Integer.parseInt(str);
+		} catch(Exception e) {
+			return defNo;
+		}
+	}
+	
+
+	
 }
