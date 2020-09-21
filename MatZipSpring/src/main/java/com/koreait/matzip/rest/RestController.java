6725d75399ce7da1f3a120ea9841d9d6b99e9d11
taddy <taddy0818@naver.com>
2020-09-21 11:27:20 +0900

    config.properties 생성 및 server-context.xml DB 부분 수정

---
+++ MatZipSpring/src/main/java/com/koreait/matzip/rest/RestController.java
@@ -0,0 +1,14 @@
+package com.koreait.matzip.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller //handlerMapper 랑 비슷함
+@RequestMapping("/rest")
+public class RestController {
+	
+	@RequestMapping(value="/restMap")
+	public String restMap() {
+		return "";
+	}
+}
--- MatZipSpring/src/main/java/com/koreait/matzip/user/UserMapper.java
+++ MatZipSpring/src/main/java/com/koreait/matzip/user/UserMapper.java
@@ -3,11 +3,13 @@ package com.koreait.matzip.user;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.koreait.matzip.user.model.UserDMI;
-import com.koreait.matzip.user.model.UserDTO;
+import com.koreait.matzip.user.model.UserPARAM;
 import com.koreait.matzip.user.model.UserVO;
 
 @Mapper // UserMapper.xml(mybatis)와 연결
 public interface UserMapper {
+	//가입
 	public int insUser(UserVO p);
-	public UserDMI selUser(UserDTO p);
+	//로그인
+	public UserDMI selUser(UserPARAM p);
 }
